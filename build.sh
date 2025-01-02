434fe2f4f6a79ce4d9c5424e899de8dae89c8c88
Lauri Tervonen <lauri.tervonen@reaktor.com>
2025-01-02 16:32:30 +0200

    Move build scripts into the theme

--- build.sh
+++ build.sh
@@ -1,17 +1,1 @@
-#!/bin/bash
-
-# Function to kill background processes when the script exits
-cleanup() {
-  echo "Cleaning up..."
-  # Kill all background processes
-  kill $(jobs -p)
-}
-trap cleanup EXIT
-
-# Start services as background processes
-
-python3 themes/MollikkaStuffMonospaceZolaTheme/renderserver/main.py &
-
-sleep 1
-
-zola build
+./themes/MollikkaStuffMonospaceZolaTheme/build.sh
